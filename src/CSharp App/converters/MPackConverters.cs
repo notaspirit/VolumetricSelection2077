06fb18cab4e29f6c44b7db68e8a125fc2ec815f9
notaspirit <unlequitbaum@gmail.com>
2025-03-19 21:47:41 +0100

    adjusted new abbrmesh structure to be mpack serializable

--- src/CSharp App/converters/MPackConverters.cs
+++ src/CSharp App/converters/MPackConverters.cs
@@ -92,6 +92,34 @@ public class QuaternionFormatter : IMessagePackFormatter<Quaternion>
     }
 }
 
+public class PlaneFormatter : IMessagePackFormatter<Plane>
+{
+    public static readonly PlaneFormatter Instance = new PlaneFormatter();
+    
+    public void Serialize(ref MessagePackWriter writer, Plane value, MessagePackSerializerOptions options)
+    {
+        writer.WriteArrayHeader(4);
+        writer.Write(value.Normal.X);
+        writer.Write(value.Normal.Y);
+        writer.Write(value.Normal.Z);
+        writer.Write(value.D);
+    }
+
+    public Plane Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options)
+    {
+        var length = reader.ReadArrayHeader();
+        if (length != 4)
+            throw new InvalidOperationException("Invalid Plane array length");
+
+        float x = reader.ReadSingle();
+        float y = reader.ReadSingle();
+        float z = reader.ReadSingle();
+        float d = reader.ReadSingle();
+
+        return new Plane(x, y, z, d);
+    }
+}
+
 // Configure MessagePack to use custom formatters
 public static class MessagePackConfig
 {
@@ -101,6 +129,7 @@ public static class MessagePackConfig
         formatters.Add(new Vector3Formatter());
         formatters.Add(new BoundingBoxFormatter());
         formatters.Add(new QuaternionFormatter());
+        formatters.Add(new PlaneFormatter());
         List<IFormatterResolver> resolvers = new List<IFormatterResolver>();
         resolvers.Add(MessagePack.Resolvers.StandardResolver.Instance);
         var customResolver = MessagePack.Resolvers.CompositeResolver.Create(
--- src/CSharp App/models/AbbrMesh.cs
+++ src/CSharp App/models/AbbrMesh.cs
@@ -1,19 +1,29 @@
+using MessagePack;
 using SharpDX;
 
 namespace VolumetricSelection2077.Models;
+
+[MessagePackObject]
 public class AbbrMesh
 {
+    [Key(0)]
     public required AbbrSubMesh[] SubMeshes { get; set; }
 }
 
+[MessagePackObject]
 public class AbbrSubMesh
 {
+    [Key(0)]
     public required BoundingBox BoundingBox { get; set; }
+    [Key(1)]
     public required Polygon[] Polygons { get; set; }
 }
 
+[MessagePackObject]
 public class Polygon
 {
+    [Key(0)]
     public required Vector3[] Vertices { get; set; }
+    [Key(1)]
     public required Plane Plane { get; set; }
 }
