cfe497c9d67203222083210dc2f50e0acafbef9a
notaspirit <unlequitbaum@gmail.com>
2025-03-22 13:27:25 +0100

    added xml documentation to logger

--- src/CSharp App/services/LoggerService.cs
+++ src/CSharp App/services/LoggerService.cs
@@ -13,6 +13,10 @@ namespace VolumetricSelection2077.Services
         private static readonly List<ILogEventSink> _sinks = new();
         private static SettingsService _settingsService;
         
+        /// <summary>
+        /// Initializes the logger service and uses any sinks that were added in the ui logger
+        /// </summary>
+        /// <param name="logDirectory">The directory to create the log file in</param>
         public static void Initialize(string logDirectory)
         {
             var logFileName = Path.Combine(logDirectory, $"log_{DateTime.Now:yyyyMMdd_HHmmss}.txt");
@@ -36,6 +40,10 @@ namespace VolumetricSelection2077.Services
             _settingsService = SettingsService.Instance;
         }
 
+        /// <summary>
+        /// Adds a sink to the list of sinks that are used on init in the ui logger
+        /// </summary>
+        /// <param name="sink">The sink to add</param>
         public static void AddSink(ILogEventSink sink)
         {
             _sinks.Add(sink);
@@ -48,10 +56,21 @@ namespace VolumetricSelection2077.Services
                     .CreateLogger();
             }
         }
-
+        
+        /// <summary>
+        /// Formats the message and level, does not account for date and time
+        /// </summary>
+        /// <param name="message">The message body</param>
+        /// <param name="level">The log level (with proper spacing)</param>
+        /// <returns>Formatted string</returns>
         private static string FormatMessage(string message, string level)
             => $"[{level}] {message}";
-
+        
+        /// <summary>
+        /// Logs an info message 
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="fileOnly">Log only to the file, but not the Ui</param>
         public static void Info(string message, bool fileOnly = false)
         {
             if (!fileOnly)
@@ -59,21 +78,35 @@ namespace VolumetricSelection2077.Services
             _fileLogger?.Information(FormatMessage(message, "Info   "));
         }
 
-
+        /// <summary>
+        /// Logs a warning message 
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="fileOnly">Log only to the file, but not the Ui</param>
         public static void Warning(string message, bool fileOnly = false) 
         {
             if (!fileOnly)
                 _uiLogger?.Warning(FormatMessage(message, "Warning"));
             _fileLogger?.Warning(FormatMessage(message, "Warning"));
         }
-
+        
+        /// <summary>
+        /// Logs an error message 
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="fileOnly">Log only to the file, but not the Ui</param>
         public static void Error(string message, bool fileOnly = false) 
         {
             if (!fileOnly)
                 _uiLogger?.Error(FormatMessage(message, "Error  "));
             _fileLogger?.Error(FormatMessage(message, "Error  "));
         }
-
+        
+        /// <summary>
+        /// Logs a debug message only if debug mode is enabled
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="fileOnly">Log only to the file, not the Ui</param>
         public static void Debug(string message, bool fileOnly = false)
         {
             if (_settingsService.DebugMode)
@@ -83,7 +116,12 @@ namespace VolumetricSelection2077.Services
                 _fileLogger?.Debug(FormatMessage(message, "Debug  "));
             }
         }
-
+        
+        /// <summary>
+        /// Logs a success message 
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="fileOnly">Log only to the file, but not the Ui</param>
         public static void Success(string message, bool fileOnly = false)
         {
             if (!fileOnly)
