eedfc0eae6b0deb42d0d8c48fb4b8fd48ed548dc
notaspirit <unlequitbaum@gmail.com>
2025-05-07 15:13:55 +0200

    added test for dialog service, added dynamic method

--- src/CSharp App/services/DialogService.cs
+++ src/CSharp App/services/DialogService.cs
@@ -9,6 +9,22 @@ namespace VolumetricSelection2077.Services;
 
 public class DialogService
 {
+    private Window _owner;
+    
+    public DialogService(Window owner)
+    {
+        _owner = owner;
+    }
+    
+    public async Task<int> ShowDialog(string title, string message, string[] buttonContents)
+    {
+        var dialog = new Dialog(title, message, buttonContents);
+        
+        await dialog.ShowDialog(_owner);
+        
+        return dialog.DialogResult;
+    }
+    
     public static async Task<int> ShowDialog(string title, string message, string[] buttonContents, Window owner)
     {
         var dialog = new Dialog(title, message, buttonContents);
