9b416712159312fc744ce7d6242e17e693de476c
notaspirit <unlequitbaum@gmail.com>
2025-07-12 17:57:25 +0200

    version bump

--- src/CET_Mod/bin/x64/plugins/cyber_engine_tweaks/mods/VolumetricSelection2077/modules/VSGui.lua
+++ src/CET_Mod/bin/x64/plugins/cyber_engine_tweaks/mods/VolumetricSelection2077/modules/VSGui.lua
@@ -14,7 +14,7 @@ local relativeOffset = vector3:new(0, 0, 0)
 local selectionBox = nil
 
 -- Settings
-local versionString = "1000.0.0-beta8"
+local versionString = "1000.0.0-beta9"
 
 local settingsInstance = settings.getInstance()
 local isHighlighted = false
